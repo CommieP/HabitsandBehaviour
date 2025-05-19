ec3256396aa2fdf4e583a5b671e739a8070c8bc4
AdityaDeCW <67094373+CommieP@users.noreply.github.com>
2025-05-19 16:05:03 +0530

    CapstonePapersUpdate

--- src/Data/CapstoneProjectsData.jsx
+++ src/Data/CapstoneProjectsData.jsx
@@ -1,7 +1,8 @@
 export const CapstoneData = [
     {
         title: "Lorem Ipsum",
-        author: "Lorem Ipsum"
+        author: "Sharon",
+        url: "/assets/capstonePapers/sharon.pdf"
     },
     {
         title: "Lorem Ipsum",
